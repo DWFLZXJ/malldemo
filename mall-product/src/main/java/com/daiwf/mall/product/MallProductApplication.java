0be1a67d13e4758a89cc07492ad862f322f8b9ea
daiwf <997940675@qq.com>
2020-09-29 21:45:27 +0800

    逆向工程的创建

--- mall-product/src/main/java/com/daiwf/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/daiwf/mall/product/MallProductApplication.java
@@ -1,8 +1,29 @@
 package com.daiwf.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+   整合mybatis-plus
+ 1)导入依赖
+ <!--mybatis-plus -->
+    <dependency>
+        <groupId>com.baomidou</groupId>
+        <artifactId>mybatis-plus-boot-starter</artifactId>
+        <version>3.3.2</version>
+    </dependency>
+    2)配置
+    1、配置数据源
+       1)导入数据库的驱动
+       2)在application.yml中配置数据源相关信息
+    2、配置mybatisplus
+    1)使用@MapperScan
+    2)告诉mybatisplus 我们的sql映射文件的位置
+    */
+
+@MapperScan("com.daiwf.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication
 {
--- mall-product/src/test/java/com/daiwf/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/daiwf/mall/product/MallProductApplicationTests.java
@@ -1,14 +1,25 @@
 package com.daiwf.mall.product;
 
+import com.daiwf.mall.product.entity.BrandEntity;
+import com.daiwf.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MallProductApplicationTests
-{
-
+class MallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
     @Test
+
     void contextLoads() {
+
+        BrandEntity brandEntity = new BrandEntity();
+       brandEntity.setBrandId(1L);
+        brandEntity.setName("华为");
+        brandEntity.setDescript("华为的新手机");
+        brandService.updateById(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
